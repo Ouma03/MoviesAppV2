03cccc5a2e114597c31a17a9c6f84d092f92108b
Oumaima Benjelloun <benjellounoumaima03@gmail.com>
2024-04-24 10:41:14 +0100

    Initial commit

---
+++ app/src/main/java/com/benjelloun/moviesappv2/MovieResponse.java
@@ -0,0 +1,11 @@
+package com.benjelloun.moviesappv2;
+
+import java.util.List;
+
+public class MovieResponse {
+    private List<MyMovieData> results;
+
+    public List<MyMovieData> getResults() {
+        return results;
+    }
+}
---
+++ app/src/main/java/com/benjelloun/moviesappv2/TmdbApi.java
@@ -0,0 +1,17 @@
+package com.benjelloun.moviesappv2;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface TmdbApi {
+    @GET("movie/popular")
+    Call<MovieResponse> getPopularMovies(@Query("api_key") String apiKey);
+
+    @GET("search/movie")
+    Call<MovieResponse> searchMovies(@Query("api_key") String apiKey,@Query("query")String query);
+
+    @GET("movie/popular")
+    Call<TrailerResponse> getMoviesTrailers(@Path("movie_id") int movieId, @Query("api_key")String apiKey);
+}
